38175c8423e7ed2a53eb2f4878fd09e828e4fd82
ifeanyi-ekpu <pauli4mary@gmail.com>
2025-03-03 08:55:59 -0500

    Initialize game environment, implement core gameplay loop, and add collision handling for player, asteroids, and shots.

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-from constants import *
 import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+import sys
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable) 
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+    
     while True:
-        screen.fill(color=(0,0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.split()
+                    
+                
+        screen.fill("black")
+
+        for object in drawable:
+            object.draw(screen)
+
+        for shot in shots:
+            shot.update(dt)
+            shot.draw(screen)
+
         pygame.display.flip()
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
